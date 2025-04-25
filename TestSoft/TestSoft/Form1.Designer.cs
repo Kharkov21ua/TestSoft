249695df4c76bc49cf43dbff0ae26c54081ebc02
Kharkov21ua <kha21ua@gmail.com>
2025-04-25 23:24:38 +0200

    1Update

--- TestSoft/TestSoft/Form1.Designer.cs
+++ TestSoft/TestSoft/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Location = new System.Drawing.Point(236, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(244, 99);
@@ -49,6 +49,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- TestSoft/TestSoft/Form1.cs
+++ TestSoft/TestSoft/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 
 namespace TestSoft
 {
@@ -15,6 +18,42 @@ namespace TestSoft
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string localVersion = "1.1"; // Текущая версия программы
+            string versionUrl = "https://raw.githubusercontent.com/Kharkov21ua/TestSoft/main/Update.txt";
+            string updateUrl = "https://raw.githubusercontent.com/Kharkov21ua/TestSoft/main/ProgramNew.exe"; // Ссылка на новый exe
+            string newFileName = "ProgramNew.exe";
+
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    string remoteVersion = client.DownloadString(versionUrl).Trim();
+
+                    if (remoteVersion != localVersion)
+                    {
+                        Console.WriteLine("Найдено обновление! Скачиваем...");
+
+                        client.DownloadFile(updateUrl, newFileName);
+
+                        Console.WriteLine("Обновление скачано. Запускаем новую версию...");
+
+                        Process.Start(newFileName); // Запустить новое приложение
+                        Environment.Exit(0); // Закрыть старую программу
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы используете последнюю версию.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка обновления: {ex.Message}");
+                }
+            }
         }
     }
 }
