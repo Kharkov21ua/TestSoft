e0cd925328912089defad21121f0b6aa0f896ce0
Kharkov21ua <kha21ua@gmail.com>
2025-04-25 23:35:26 +0200

    1

--- TestSoft/TestSoft/Form1.cs
+++ TestSoft/TestSoft/Form1.cs
@@ -22,38 +22,7 @@ namespace TestSoft
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string localVersion = "1.2"; // Текущая версия программы
-            string versionUrl = "https://github.com/Kharkov21ua/TestSoft/blob/main/TestSoft/TestSoft/bin/Debug/Update.txt";
-            string updateUrl = "https://github.com/Kharkov21ua/TestSoft/blob/main/TestSoft/TestSoft/bin/Debug/TestSoft.exe"; // Ссылка на новый exe
-            string newFileName = "ProgramNew.exe";
-
-            using (var client = new WebClient())
-            {
-                try
-                {
-                    string remoteVersion = client.DownloadString(versionUrl).Trim();
-
-                    if (remoteVersion != localVersion)
-                    {
-                        Console.WriteLine("Найдено обновление! Скачиваем...");
-
-                        client.DownloadFile(updateUrl, newFileName);
-
-                        Console.WriteLine("Обновление скачано. Запускаем новую версию...");
-
-                        Process.Start(newFileName); // Запустить новое приложение
-                        Environment.Exit(0); // Закрыть старую программу
-                    }
-                    else
-                    {
-                        Console.WriteLine("Вы используете последнюю версию.");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Ошибка обновления: {ex.Message}");
-                }
-            }
+            
         }
     }
 }
