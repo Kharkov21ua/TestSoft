b133b2ae341e355c5aae36b89513329d8fd6b26e
Kharkov21ua <kha21ua@gmail.com>
2025-04-25 23:50:11 +0200

    commiy2

--- TestSoft/TestSoft/Form1.cs
+++ TestSoft/TestSoft/Form1.cs
@@ -23,7 +23,16 @@ namespace TestSoft
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if (client.DownloadFile)
+            if (client.DownloadString("https://github.com/Kharkov21ua/TestSoft/blob/main/TestSoft/TestSoft/bin/Debug/Update.txt").Contains("1.0"))
+            {
+                // Пропускаем
+                MessageBox.Show("Новейшая версия", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Обновите софт","Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
         }
     }
 }
