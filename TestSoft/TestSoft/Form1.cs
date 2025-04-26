3b3b1dd1524a33d8bc0ad025a4a05c5ce479159b
Kharkov21ua <kha21ua@gmail.com>
2025-04-26 15:21:49 +0200

    1.4

--- TestSoft/TestSoft/Form1.cs
+++ TestSoft/TestSoft/Form1.cs
@@ -26,7 +26,7 @@ namespace TestSoft
             {
                 
 
-                if (client.DownloadString("https://pastebin.com/G8X1Ve5c").Contains("1.3"))
+                if (client.DownloadString("https://pastebin.com/G8X1Ve5c").Contains("1.4"))
                 {
                     MessageBox.Show("Новейшая версия", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
